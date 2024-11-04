0ca023c3521c2ae85c34d22bc21001593ff489f4
Thomas Peugnet <thomas.peugnet.pro@gmail.com>
2024-11-05 00:59:41 +0100

    ``` feat(config): Update GPT prompt for improved response accuracy ```

--- cohabot-backend/src/controller.js
+++ cohabot-backend/src/controller.js
@@ -1,4 +1,4 @@
-const { generateResponse } = require('./service');
+const {generateResponse} = require('./service');
 const {gpt_max_token_input} = require("./config/dotenv");
 
 /**
@@ -7,25 +7,31 @@ const {gpt_max_token_input} = require("./config/dotenv");
  * @param res
  * @returns {*}
  */
-exports.getResponse = async (request, res) => {
+exports.getResponse = async (request, res) =>
+{
     const userMessage = request.body.message;
 
     // Check if the message is missing
-    if (!userMessage) {
-        return res.status(400).json({ error: "Vous devez envoyer un message non vide." });
+    if (!userMessage)
+    {
+        return res.status(400).json({error: "Your message seems to be empty."});
     }
 
     // Check if the message is not too long (ChatGPT costs limits)
-    if (userMessage.length > gpt_max_token_input) {
-        return res.status(400).json({ error: "Votre message est trop long pour être correctement traité." });
+    if (userMessage.length > gpt_max_token_input)
+    {
+        return res.status(400).json({error: "Your message is too long to be sent like this."});
     }
 
-    try {
+    try
+    {
         // Generate the response asynchronously
         const response = await generateResponse(userMessage);
-        res.json({ response });
-    } catch (error) {
+        res.json({response});
+    }
+    catch (error)
+    {
         console.error("Error while generating chatbot response:", error.message);
-        res.status(500).json({ error: "Une erreur est survenue lors de la génération de la réponse. Merci de réessayer plus tard." });
+        res.status(500).json({error: "An error occured while speaking to the FAQ agent. Please try again later."});
     }
 };
