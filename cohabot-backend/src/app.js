e1cd9b435fbeadb2580e9a836479cdf0d3a00ba3
Thomas Peugnet <thomas.peugnet.pro@gmail.com>
2024-11-06 00:49:47 +0100

    feat(docker): switch to alpine base image, add front_url config

--- cohabot-backend/src/app.js
+++ cohabot-backend/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const {front_url} = require("./config/dotenv");
+
 
 const app = express();
 
 // Configure CORS to allow connections from localhost:3000
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: front_url
 }));
 
 app.use(express.json());
--- cohabot-backend/src/config/dotenv.js
+++ cohabot-backend/src/config/dotenv.js
@@ -4,4 +4,5 @@ module.exports = {
     port: process.env.PORT,
     gpt_max_token_input: process.env.GPT_MAX_TOKENS_INPUT,
     openai_api_key: process.env.OPENAI_API_KEY,
+    front_url: process.env.FRONT_URL,
 };
