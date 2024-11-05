c6f05d5de98abc73f0751a806e6d5dec38f406bc
Thomas Peugnet <thomas.peugnet.pro@gmail.com>
2024-11-05 23:50:16 +0100

    feat(dependencies): add cors package

--- cohabot-backend/src/app.js
+++ cohabot-backend/src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+// Configure CORS to allow connections from localhost:3000
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
 app.use(express.json());
 
 // Routes
